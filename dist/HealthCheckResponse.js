0ee5cc4c6e03685318876e2db49159cce7d346c5
Marcos R. Araujo <marquinhoaraujo@gmail.com>
2021-12-13 21:22:13 -0300

    chore: delete dist dir

--- dist/HealthCheckResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HealthCheckResponse {
-    constructor(status = "UP") {
-        this.status = "Any Status";
-        this.status = status;
-    }
-}
-exports.default = HealthCheckResponse;
