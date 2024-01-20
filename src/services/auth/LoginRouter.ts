133a35bac1bbc8a97938ca209c20ac70b928b160
exprcode <expressivecode@gmail.com>
2024-01-20 16:43:14 -0300

    refactor: change auth endpoints path

--- src/services/auth/LoginRouter.ts
+++ src/services/auth/LoginRouter.ts
@@ -4,7 +4,7 @@ import { ILoginRequest, ILoginResponse } from './model/LoginDTO'
 import { LoginDTO, LoginModel } from './model'
 
 export default Router()
-  .post('/signin', async (req: Request, res: Response): Promise<Response> => {
+  .post('/register', async (req: Request, res: Response): Promise<Response> => {
     const loginData: ILoginRequest = req.body as ILoginRequest
 
     const loginDTO = LoginDTO.create(loginData.username, loginData.password)
@@ -17,7 +17,7 @@ export default Router()
 
     return res.status(201).json(result)
   })
-  .post('/login', async (req: Request, res: Response): Promise<Response> => {
+  .post('/signin', async (req: Request, res: Response): Promise<Response> => {
     const loginData = req.body
     const user = await LoginModel.findOne({
       username: loginData.username,
